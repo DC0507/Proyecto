d2ffd07b8cbd552a40e240499c4e64251c8fbc5c
David Calderon <dcalderon179105@gmail.com>
2026-03-23 14:46:21 -0600

    Creación del archivo producto.js
    Se creo el archivo para el componente producto.js

---
+++ views/components/producto.js
@@ -0,0 +1,16 @@
+//Crear un componente de producto que reciba un objeto con las propiedades: 
+// nombre, precio y peso. El componente debe mostrar su nombre, precio peso. 
+// Además, debe incluir los botones "Agregar al carrito" y "Ver detalles" que al hacer clic 
+
+<div class="product-card">
+        <img
+          class="product-img"
+          src="#"
+        />
+        <p class="product-details">
+          <span class="product-name">NOMBRE</span>
+          <span class="product-price">Precio: ₡PRECIO</span>
+        </p>
+        <button class="product-btn-add">+Agregar</button>
+        <button class="product-btn-details">Detalles</button>
+      </div>
